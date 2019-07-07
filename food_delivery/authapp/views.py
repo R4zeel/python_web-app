9b6903baaf5c657e40a49f99179594bf664478f6
bitoffluff1 <lena_khmelnikova@mail.ru>
2019-07-07 10:14:23 +0300

    Добавлены шаблоны для коризны,и сделал logout для authapp.

---
+++ food_delivery/authapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+def logout (request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse( 'main' ))
